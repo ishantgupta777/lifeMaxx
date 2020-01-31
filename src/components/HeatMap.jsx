42cccbf9861c51c2a9191814114c40bf3809b70d
Ishantgupta777 <Ishantgupta777@gmail.com>
2020-01-31 08:52:03 +0530

    clustering of markers added

--- src/components/HeatMap.jsx
+++ src/components/HeatMap.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import React, { useState, useEffect, useRef, Fragment } from 'react';
+import ReactMapGL, { Marker,FlyToInterpolator } from 'react-map-gl';
 import axios from 'axios';
+import './HeatMap.css'
+import useSupercluster from 'use-supercluster'
 
 const MAPBOX_TOKEN =
 	'pk.eyJ1IjoiaXNoYW50Z3VwdGE3NzciLCJhIjoiY2p5NDI5aXFpMTVvaDNnbGVhbTllZ2R3MyJ9.ndww9Z602MqyVtoiexGXqQ';
@@ -11,7 +13,7 @@ export default function HeatMap() {
 		height: '85vh',
 		latitude: 37.7577,
 		longitude: -122.4376,
-		zoom: 15
+		zoom: 2
 	});
 
 	const [ currentPosition, setCurrentPosition ] = useState({
@@ -40,41 +42,114 @@ export default function HeatMap() {
 		getLocation();
 
 		const getMissingPeople = async () => {
-			const res = await axios.get('/unsafePeople');
-			setMissingPeople(res.data);
+			var res = await axios.get('/unsafePeople');
+			res = res.data.map((person) => {
+				return {
+					type: 'Feature',
+					properties: {
+						cluster: false,
+						personId: person._id,
+						category: 'missing people'
+					},
+					geometry: { type: 'Point', coordinates: person.lastLocation.reverse() }
+				};
+			});
+
+			setMissingPeople(res);
 		};
 		getMissingPeople();
 	}, []);
 
+	const mapRef = useRef();
+
+	const bounds = mapRef.current ? mapRef.current.getMap().getBounds().toArray().flat() : null;
+
+	const { clusters, supercluster } = useSupercluster({
+		points : missingPeople,
+		bounds,
+		zoom: viewport.zoom,
+		options: { radius: 75, maxZoom: 20 }
+	  });
+	  
 	return (
-		<ReactMapGL
-			{...viewport}
-			mapboxApiAccessToken={MAPBOX_TOKEN}
-			onViewportChange={setViewport}
-			mapStyle="mapbox://styles/ishantgupta777/ck5zjfpzo0ysy1ipccqjbo3ix"
-			maxZoom={20}
-		>
-			{currentPosition.lat && (
-				<Marker latitude={currentPosition.lat} longitude={currentPosition.long} draggable>
-					<img
-						src={require('../assets/img/blue_map_marker.png')}
-						alt="current_location"
-						style={{ width: '30px' }}
-					/>
-				</Marker>
-			)}
-			{missingPeople.map((person) => {
-				console.log(person.lastLocation.lat);
-				return (
-					<Marker latitude={person.lastLocation[0]} longitude={person.lastLocation[1]}>
+		<Fragment>
+			<ReactMapGL
+				{...viewport}
+				mapboxApiAccessToken={MAPBOX_TOKEN}
+				onViewportChange={setViewport}
+				mapStyle="mapbox://styles/ishantgupta777/ck5zjfpzo0ysy1ipccqjbo3ix"
+				maxZoom={20}
+				ref={mapRef}
+			>
+				{currentPosition.lat && (
+					<Marker latitude={currentPosition.lat} longitude={currentPosition.long} draggable>
 						<img
-							src={require('../assets/img/red_map_marker.png')}
+							src={require('../assets/img/blue_map_marker.png')}
 							alt="current_location"
 							style={{ width: '30px' }}
 						/>
 					</Marker>
-				);
-			})}
-		</ReactMapGL>
+				)}
+				{clusters.map(cluster => {
+					const [longitude, latitude] = cluster.geometry.coordinates;
+					const {
+					cluster: isCluster,
+					point_count: pointCount
+					} = cluster.properties;
+					console.log(viewport.zoom);
+					// we have a cluster to render
+					if (isCluster) {
+					return (
+						<Marker
+						key={`cluster-${cluster.id}`}
+						latitude={latitude}
+						longitude={longitude}
+						>
+						<div
+							className="cluster-marker"
+							style={{
+							width: `${20 + (pointCount / missingPeople.length) * 20}px`,
+							height: `${20 + (pointCount / missingPeople.length) * 20}px`
+							}}
+							onClick={() => {
+								const expansionZoom = Math.min(supercluster.getClusterExpansionZoom(cluster.id),20)
+						  
+								setViewport({
+								  ...viewport,
+								  latitude,
+								  longitude,
+								  zoom: expansionZoom,
+								  transitionInterpolator: new FlyToInterpolator({
+									speed: 2
+								  }),
+								  transitionDuration: "auto"
+								});
+							  }}
+						>
+							{pointCount}
+						</div>
+						</Marker>
+					);
+					}
+
+					return (
+					<Marker
+						key={`person-${cluster.properties.personId}`}
+						latitude={latitude}
+						longitude={longitude}
+					>
+						<button className="person-marker">
+								<img
+							src={require('../assets/img/red_map_marker.png')}
+							alt="current_location"
+							style={{ width: '30px' }}
+							draggable={false}
+						/>
+						</button>
+					</Marker>
+					);
+				})}
+			</ReactMapGL>
+		</Fragment>
 	);
 }
